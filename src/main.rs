fb1a32ebec6c7bfeb54487206ecdd813fd1f10d7
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2019-04-04 12:15:43 -0700

    :shirt:

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
 extern crate regex;
 
-use std::env;
 use regex::Regex;
+use std::env;
 
 fn main() {
     let chars_pattern = Regex::new(r"[^a-z0-9]+").unwrap();
     let args: Vec<String> = env::args().collect();
     let mut text: String = args[1..].join(" ").trim().to_lowercase();
-    text =
-        chars_pattern
+    text = chars_pattern
         .replace_all(&text, "-")
         .trim_end_matches("-")
         .trim_start_matches("-")
