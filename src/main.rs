73db97b18d32b884b7ddb46fd958aabad0273fef
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2019-04-04 06:58:43 -0700

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+extern crate regex;
+
+use std::env;
+use regex::Regex;
+
+fn main() {
+    let chars_pattern = Regex::new(r"[^a-z0-9]+").unwrap();
+    let args: Vec<String> = env::args().collect();
+    let mut text: String = args[1..].join(" ").trim().to_lowercase();
+    text = chars_pattern.replace_all(&text, "-").to_string();
+    text = text.trim_end_matches("-").trim_start_matches("-").to_string();
+
+    println!("{}", text);
+}
