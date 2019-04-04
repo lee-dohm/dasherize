073d247e70a731590392c038d3ac4683d3273c2f
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2019-04-04 13:06:01 -0700

    Create functions to encapsulate and learn function signatures

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,27 @@ use regex::Regex;
 use std::env;
 
 fn main() {
+    let mut text: String = get_arg_text();
+    text = dasherize(text);
+
+    println!("{}", text);
+}
+
+/// Returns the dasherized version of the given text.
+fn dasherize(mut text: String) -> String {
     let chars_pattern = Regex::new(r"[^a-z0-9]+").unwrap();
-    let args: Vec<String> = env::args().collect();
-    let mut text: String = args[1..].join(" ").trim().to_lowercase();
-    text = chars_pattern
+    text = text.trim().to_lowercase();
+
+    chars_pattern
         .replace_all(&text, "-")
         .trim_end_matches("-")
         .trim_start_matches("-")
-        .to_string();
+        .to_string()
+}
 
-    println!("{}", text);
+/// Gets the text passed as arguments on the command line, returning it as one string.
+fn get_arg_text() -> String {
+    let args: Vec<String> = env::args().collect();
+
+    args[1..].join(" ")
 }
